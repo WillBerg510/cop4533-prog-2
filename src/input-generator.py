01bcd56c7753f97642e9da0f3789efdc70d22e45
Will Berg <willberg510@gmail.com>
2026-03-06 15:00:18 -0500

    Allow input generator to work with uniform and normal distributions

--- src/input-generator.py
+++ src/input-generator.py
@@ -10,10 +10,15 @@ while (not m.isdigit()) or int(m) < 0:
   m = input("m must be a non-negative integer.\nInput m (number of requests): ")
 m = int(m)
 
+dist = input("Input type of distribution (u for uniform, n for normal): ")
+while (dist != "u" and dist != "n"):
+  dist = input("Invalid input.\nInput type of distribution (u for uniform, n for normal): ")
+
 with open("../data/generated.in", "w") as file:
   file.write(f"{k} {m}")
   file.write("\n")
-  for i in range(0, m): # Write m random numbers in the range (1, 2k)
-    file.write(f"{random.randint(1, 2 * k)}")
+  for i in range(0, m):
+    num = round(random.normalvariate(100, 10)) if dist == "n" else round(random.uniform(75, 125))
+    file.write(f"{num}")
     if (i != m - 1):
       file.write(" ")
