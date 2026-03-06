f2fef7d294dc7f15651b16356047de1b608554f2
Lam Nguyen <149017745+lamukaseedu@users.noreply.github.com>
2026-03-06 01:26:33 -0500

    Clean up code, create LRU function

--- src/main.py
+++ src/main.py
@@ -2,13 +2,27 @@
 def fifo(k: int, m: int, r: list[int]) -> int:
     cache = []
     miss = 0
-    for i in range(0, m):
-        value = r[i % len(r)]
+    for value in r:
         if value not in cache:
             cache.append(value)
             miss += 1
             if len(cache) > k:
                 cache.pop(0)
+
+    return miss
+
+def lru(k: int, m: int, r: list[int]) -> int:
+    cache = []
+    miss = 0
+    for value in r:
+        if value not in cache:
+            cache.append(value)
+            miss += 1
+            if len(cache) > k:
+                cache.pop(0)
+        else:
+            cache.pop(cache.index(value))
+            cache.append(value)
 
     return miss
 
@@ -36,8 +50,12 @@ if __name__ == "__main__":
     fifo_result = fifo(k, m, r)
 
     # LRU
+    lru_result = lru(k, m, r)
 
     # OPTFF
+    optff_result = "TODO"
 
     file.close()
-    print("FIFO: ", fifo_result)
+    print("FIFO  : ", fifo_result)
+    print("LRU   : ", lru_result)
+    print("OPTFF : ", optff_result)
